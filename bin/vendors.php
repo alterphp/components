79af266904dd899ca5220604205c3224752e72af
PéCé <pc.bertineau@gmail.com>
2012-02-06 22:45:21 +0100

    BitTools + EntityBit form field type

--- bin/vendors.php
+++ bin/vendors.php
@@ -12,6 +12,10 @@ if (!is_dir(VENDOR_PATH)) {
 $deps = array(
     array('Symfony/Component/ClassLoader', 'http://github.com/symfony/ClassLoader.git', 'master'),
     array('Symfony/Component/HttpFoundation', 'http://github.com/symfony/HttpFoundation.git', 'master'),
+    array('Symfony/Component/Form', 'http://github.com/symfony/Form.git', 'master'),
+    array('Symfony/Bridge/Doctrine', 'http://github.com/symfony/DoctrineBridge.git', 'master'),
+    array('Doctrine/ORM', 'https://github.com/doctrine/doctrine2.git', 'master'),
+    array('Doctrine/Common', 'https://github.com/doctrine/common.git', 'master'),
 );
 
 foreach ($deps as $dep) {
